720b4f63b272a9f15aee990554c41a4ac730294f
PolyaneTuag <pollyportugues@hotmail.com>
2020-06-07 17:33:27 -0300

    Commit inicial

---
+++ Aulajs/index.js
@@ -0,0 +1,27 @@
+//Criar um programa que calcula a média
+//das notas entre os alunos e envia
+// mensagem do cálculo da média
+
+
+const aluno01 = 'Mayk'
+const notaAluno01 = 9.8
+
+const aluno02 = 'Diego'
+const notaAluno02 = 10
+
+const aluno03 = 'Luis'
+const notaAluno03 = 2
+
+
+
+const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
+
+//Se a média for maior que 5, parabenizar a turma
+if (media > 5) {
+    console.log(`A média foi de ${media}. Parabéns!`)
+} else {
+    console.log('A média é menor que 5')
+}
+
+
+console.log(media)
